09906393f7f757d5b19a40373be65303191b75a7
devMantse <devmantse@gmail.com>
2023-10-02 19:40:05 +0000

    DOM manipulation with forms

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ const form = document.getElementById('myForm');
+    const output = document.getElementById('output');
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+
+      output.innerHTML = `Hello, ${name}! Your email is ${email}.`;
+    });
