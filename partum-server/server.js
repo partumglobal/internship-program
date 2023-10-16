019b89b22c0b75d9af2a46a73c03c3a236776e4b
devMantse <devmantse@gmail.com>
2023-10-16 12:22:41 +0000

    node modules ignored

--- partum-server/server.js
+++ partum-server/server.js
@@ -1,89 +1,63 @@
-import express from 'express'
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// !TODO install dependencies
-
-// npm install
-
-// npm init
-// npm install express
-// node server.js
+const bodyParser = require('body-parser');
 
 app.use(express.json());
 
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send('Something broke!');
-// });
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 let todos = [
-    {
-        id: 1 ,
-        title: 'My first todo',
-        complete: true
-    },
-     {
-        id: 2 ,
-        title: 'My second todo',
-        complete: false
-    }
-]
+  { id: 1, title: 'Learn Node.js', completed: false },
+  { id: 2, title: 'Build a RESTful API', completed: false }
+];
 
-
-// define a route
-
-// GET ROUTE
 app.get('/todos', (req, res) => {
-    res.json(todos)
-})
+  res.json(todos);
+});
 
-// GET BY ID ROUTE
 app.get('/todos/:id', (req, res) => {
-   const id = parseInt(req.params.id)
-   const todo = todos.find(todo => todo.id === id);
+  const id = parseInt(req.params.id);
+  const todo = todos.find(todo => todo.id === id);
 
-   if (todo){
-    res.json(todo)
-   } else {
-    res.status(404).json({message: "Todo not found"})
-   }
-})
+  if (todo) {
+    res.json(todo);
+  } else {
+    res.status(404).json({ message: 'Todo not found' });
+  }
+});
 
-// DELETE BY ID ROUTE
+app.post('/todos', (req, res) => {
+  const { title, completed } = req.body;
+  const id = todos.length + 1;
+  const newTodo = { id, title, completed: completed || false };
+  todos.push(newTodo);
+  res.json(newTodo);
+});
+
+
+app.put('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, completed } = req.body;
+  const todo = todos.find(todo => todo.id === id);
+
+  if (todo) {
+    todo.title = title || todo.title;
+    todo.completed = completed || todo.completed;
+    res.json(todo);
+  } else {
+    res.status(404).json({ message: 'Todo not found' });
+  }
+});
+
 app.delete('/todos/:id', (req, res) => {
-   const id = parseInt(req.params.id)
-   const todo = todos.findIndex(todo => todo.id === id)
+  const id = parseInt(req.params.id);
+  todos = todos.filter(todo => todo.id !== id);
+  res.json({ message: 'Todo deleted' });
+});
 
-    if (todo !== -1 ){
-        todos.splice(todo, 1)
-
-        res.json({message: `todo with ${id} deleted`})
-    } else {
-    res.status(404).json({message: "Todo not found"})
-    }
-})
-
-
-// !TODO implement PUT AND POST routes
-
-// POST ROUTE
-
-// request body
-// push to todo array
-// response
-
-
-// PUT ROUTE
-
-// get todo id
-// request body
-// compare todos
-
-
-
-
-app.listen( port, () => {
-    console.log(`server is running on port ${port} ` );
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
